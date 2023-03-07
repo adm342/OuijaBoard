5c36419d9300e3217c47b323d09efc9737928c30
Aaron McManus <am1342d@gmail.com>
2023-03-07 12:24:55 -0500

    Connected Project to GitHub Repo

---
+++ OuijaBoardApp.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class OuijaBoardApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner();
+        System.out.println("Welcome to the Ouija Board");
+    }
+}
